2a3d979b2d5a7efd5fb9e2a98bae644bafcbab4f
George Asiedu <georgeasiedu543@gmail.com>
2024-01-09 21:54:51 +0000

    before: toggle functionality

---
+++ src/js/app.js
@@ -0,0 +1,14 @@
+console.log("hi");
+
+const allQuestionEls = document.querySelectorAll(".faq-accordion__subtitle");
+
+const toggleAnswer = (e) => {
+  const answer = e.target.closest(
+    ".faq-accordion__question"
+  ).nextElementSibling;
+  answer.classList.toggle("show");
+};
+
+allQuestionEls.forEach((el) => {
+  el.addEventListener("click", toggleAnswer);
+});
